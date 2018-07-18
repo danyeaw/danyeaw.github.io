4d80304ceb3940d085a56766a82fa425c7ca3f7c
Dan Yeaw <dan@yeaw.me>
2018-07-17 21:51:04 -0400

    Nikola auto commit.
    Nikola version: 7.8.15

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Dan Yeaw"  # (translatable)
 BLOG_TITLE = "Dan Yeaw's Site"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://danyeaw.github.io/"
+SITE_URL = "https://dan.yeaw.me/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://danyeaw.github.io/"
